55ce6cd366e7cc65b26b7f63a39048342403371a
lantomalalaWork <justin.lantomalala@smartone.ai>
2025-04-14 11:33:23 +0300

    delet chrome-aws-lambda and use some params sur puppeter

--- server.js
+++ server.js
@@ -2,14 +2,27 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/api/facebook-info", async (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: "Missing 'url' parameter" });
 
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+        "--disable-gpu"
+      ]
+    });
+
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "domcontentloaded" });
 
@@ -17,13 +30,10 @@ app.get("/api/facebook-info", async (req, res) => {
     const profilImage = await page.$eval('meta[property="og:image"]', el => el.content);
 
     await browser.close();
-
     res.json({ name, profilImage });
   } catch (error) {
     res.status(500).json({ error: "Failed to extract data", details: error.message });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`✅ Server started on port ${PORT}`));
